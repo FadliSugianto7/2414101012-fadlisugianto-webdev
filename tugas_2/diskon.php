3eb1c28541daecd56b3d53c9f8ee146e87f1bcc5
Fadli Sugianto <sugiantofadli685@gmail.com>
2025-12-18 20:16:55 +0700

    Add files via upload

---
+++ tugas_2/diskon.php
@@ -0,0 +1,22 @@
+<?php
+
+function hitungDiskon($totalBelanja)
+{
+    $diskon = 0;
+
+    if ($totalBelanja >= 100000) {
+        $diskon = 0.10 * $totalBelanja;
+    } elseif ($totalBelanja >= 50000) {
+        $diskon = 0.05 * $totalBelanja;
+    }
+
+    return $diskon;
+}
+
+$totalBelanja = 120000;
+$diskon       = hitungDiskon($totalBelanja);
+$totalBayar   = $totalBelanja - $diskon;
+
+echo "Total Belanja : Rp. " . $totalBelanja . "<br>";
+echo "Diskon        : Rp. " . $diskon . "<br>";
+echo "Total Bayar   : Rp. " . $totalBayar . "<br>";
